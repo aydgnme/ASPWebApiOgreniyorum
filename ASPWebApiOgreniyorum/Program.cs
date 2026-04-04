150127f1c4ef5b21f8e1f2751f96d39e86a78765
Ioan Mert Aydogan <77471284+aydgnme@users.noreply.github.com>
2026-04-04 08:00:28 +0000

    Ders 7: Part 2 - EF Core Migration tamamlandı ve SQLite veritabanı oluşturuldu

--- ASPWebApiOgreniyorum/Program.cs
+++ ASPWebApiOgreniyorum/Program.cs
@@ -1,3 +1,6 @@
+using ASPWebApiOgreniyorum.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Ek hizmetleri ekleyin
@@ -8,6 +11,12 @@ builder.Services.AddSwaggerGen();
 // Bu satırlar, Swagger'ı etkinleştirir ve API'nizin belgelenmesini sağlar. Swagger, API'nizin nasıl kullanılacağını görsel olarak göstermek için kullanılır.
 // dotnet add package Swashbuckle.AspNetCore
 // Bu komut, Swashbuckle.AspNetCore paketini projenize ekler. Bu paket, Swagger'ı ASP.NET Core uygulamanızda kullanmanızı sağlar.
+
+// DBContext ekleme
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
+    // Bu satır, Entity Framework Core'un DbContext'ini uygulamanıza ekler ve SQLite veritabanını kullanarak bağlantı sağlar. "DefaultConnection" adlı bağlantı dizesi, appsettings.json dosyanızda tanımlanmalıdır.
+);
 
 var app = builder.Build();
 
--- ASPWebApiOgreniyorum/obj/Debug/net10.0/ASPWebApiOgreniyorum.AssemblyInfo.cs
+++ ASPWebApiOgreniyorum/obj/Debug/net10.0/ASPWebApiOgreniyorum.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASPWebApiOgreniyorum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa05dfef5feddfa639bc9474636a257e3b1a4eb7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9497d4df4c74b12f71b2316a7e30fc2582ddd947")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASPWebApiOgreniyorum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASPWebApiOgreniyorum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
